8303b9489044e9d53b8bac3f1ecda41270a61f56
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-09-11 17:14:08 -0400

    create TableViewCell, connect cocoapods and add images

--- Flix/Controller/ViewController.swift
+++ Flix/Controller/ViewController.swift
@@ -6,32 +6,59 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
     var movies = [[String:Any]]()
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 150
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(ProcessInfo.processInfo.environment["API_KEY"] ?? "")")!
         let request = URLRequest(url:url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { [self] (data, response, error) in
                  
-                 if let error = error {
+        if let error = error {
                      
-                        print(error.localizedDescription)
+            print(error.localizedDescription)
                      
-                 } else if let data = data {
+        } else if let data = data {
                      
-                        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                        self.movies = dataDictionary["results"] as! [[String:Any]]
-            
-                 }
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            self.movies = dataDictionary["results"] as! [[String:Any]]
+            self.tableView.reloadData()
             }
-            task.resume()
+        }
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+      
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
+        cell.posterView.af_setImage(withURL: posterURL!)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
     }
 
 
