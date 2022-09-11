c187885ac4134953db7421fa589485c2bc8a7121
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-09-11 15:52:21 -0400

    create network request

--- Flix/Controller/ViewController.swift
+++ Flix/Controller/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var movies = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(ProcessInfo.processInfo.environment["API_KEY"] ?? "")")!
+        let request = URLRequest(url:url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { [self] (data, response, error) in
+                 
+                 if let error = error {
+                     
+                        print(error.localizedDescription)
+                     
+                 } else if let data = data {
+                     
+                        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                        self.movies = dataDictionary["results"] as! [[String:Any]]
+            
+                 }
+            }
+            task.resume()
     }
 
 
